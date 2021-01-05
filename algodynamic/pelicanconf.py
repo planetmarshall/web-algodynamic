91cf4119948e6abce1bb3c02afc4203a759740e7
Andrew Marshall <planetmarshalluk@gmail.com>
2021-01-05 22:42:29 +0000

    add livereload to requirements

--- algodynamic/pelicanconf.py
+++ algodynamic/pelicanconf.py
@@ -51,5 +51,5 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 STATIC_PATHS = ["js"]
